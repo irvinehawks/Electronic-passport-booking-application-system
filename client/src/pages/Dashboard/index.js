b057e27fe9e522e7d8fdae8c8c8a6c315391597c
Irvene Kwambana <irvenehawks@gmail.com>
2023-06-18 20:03:10 +0200

    Added guidance and resources modules to the dashboard

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,8 +1,10 @@
 import AddJob from "./AddJob/AddJob";
+import Guidance from "./Guidance/Guidance";
+import Resources from "./Resources/Resources";
 import AddBooking from "./AddBooking/AddBooking";
 import AllJobs from "./AllJobs";
 import Profile from "./Profile/Profile";
 import Stats from "./Stats";
 import SharedLayout from "./SharedLayout/SharedLayout";
 
-export { AllJobs, Profile, SharedLayout, Stats, AddJob, AddBooking };
+export { AllJobs, Profile, SharedLayout, Stats, AddJob, AddBooking, Guidance, Resources };
--- client/src/pages/landingPage/Landing.jsx
+++ client/src/pages/landingPage/Landing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import main from "../../assets/images/main.svg";
+import epassport from "../../assets/images/epassport.jpg";
 import { Wrapper } from "./landing.styles";
-import { Logo } from "../../components/index";
+//import { Logo } from "../../components/index";
 import { Link } from "react-router-dom";
 
 const Landing = () => {
@@ -9,21 +9,30 @@ const Landing = () => {
     <Wrapper>
       <nav>
         {/*<Logo />*/}
-        <p>Welcome on board. Acquire you passport travel documents the smart way.</p>
+
+        <h4>Welcome on board.</h4>
+
       </nav>
 
       <div className="container page">
         {" "}
         <div className="info">
           <h1>
-            <span>Electronic passport application web platform</span>
+            <span>Electronic passport booking and application platform</span>
           </h1>
-          <p>Apply for your e-passport of choice at the comfort of your homes now!</p>
+          <p>
+            Book and apply for your e-passport of choice at the comfort of your
+            homes now!
+          </p>
           <Link to="/register" className="btn btn-hero">
             Login/Register
           </Link>
         </div>
-        <img src={main} alt="job hunt" className="img main-img" />
+        <img
+          src={epassport}
+          alt="E-passport application"
+          className="img main-img"
+        />
       </div>
     </Wrapper>
   );
